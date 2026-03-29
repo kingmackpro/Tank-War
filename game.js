27f37b8e76cbf42818272a2fd80b1809a9b72a44
kingmackpro <oprangerx@gmail.com>
2026-03-29 15:55:36 +0530

    url up

--- game.js
+++ game.js
@@ -2,7 +2,7 @@ const canvas = document.getElementById("game");
 const ctx = canvas.getContext("2d");
 
 /* SERVER URL */
-const SERVER_URL = "wss://grip-spreading-ordinance-blonde.trycloudflare.com";
+const SERVER_URL = "wss://prospective-cos-crimes-unique.trycloudflare.com";
 
 let socket;
 let sessionId = localStorage.getItem("tankSession");
