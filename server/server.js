67e8b0933bdf70a489fbeaef836efd112f7bbf8e
kingmackpro <oprangerx@gmail.com>
2026-09-19 16:24:07 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -143,7 +143,7 @@ const weaponSystem = createWeaponSystem({
   weaponDefinitions
 });
 
-wws.on("connection", (ws) => {
+wss.on("connection", (ws) => {
 let player = null;
 let playerId = null;
 let windowStart = Date.now();
